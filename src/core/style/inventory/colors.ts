83ec344e52f8c4ad4b9af3360c8e64fc9a46f4a3
Bas Kaal <3askaal@gmail.com>
2023-08-22 17:19:14 +0200

    change theme colors config

--- src/core/style/inventory/colors.ts
+++ src/core/style/inventory/colors.ts
@@ -2,7 +2,7 @@ import { mix } from 'chroma-js'
 import { darken, brighten, rgba } from '../utils'
 
 interface Colors {
-  [variant: string]: string
+  [variant: string]: string | Colors
 }
 
 const white: string = darken('white', 0.4)
@@ -15,21 +15,21 @@ const secondary: string = '#7c73e6'
 const secondaryDark: string = darken(secondary, 0.4)
 
 const greys: any = {
-  grey10: darken('white', 0.5),
-  grey20: darken('white', 1),
-  grey30: darken('white', 1.5),
-  grey40: darken('white', 2),
-  grey50: darken('white', 2.5),
-  grey60: darken('white', 3),
-  grey70: darken('white', 3.5),
-  grey80: darken('white', 4),
-  grey90: darken('white', 4.5),
+  10: darken('white', 0.5),
+  20: darken('white', 1),
+  30: darken('white', 1.5),
+  40: darken('white', 2),
+  50: darken('white', 2.5),
+  60: darken('white', 3),
+  70: darken('white', 3.5),
+  80: darken('white', 4),
+  90: darken('white', 4.5),
 }
 
 export const colors: Colors = {
   white,
   black,
-  ...greys,
+  greys,
   primary,
   primaryDark,
   secondary,
