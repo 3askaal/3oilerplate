44827baded4963beea2d7d5b0125e77aa487c764
Bas Kaal <3askaal@gmail.com>
2023-11-05 17:25:16 +0100

    fix issue with theme provider wrapper

--- src/core/utils/provider.tsx
+++ src/core/utils/provider.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { ThemeProvider as OriginalThemeProvider } from 'styled-components'
 import deepmerge from 'deepmerge'
-import { theme as LOCAL_THEME } from "../style/theme";
+import { theme as DEFAULT_THEME } from "../style/theme";
 import { STheme } from '../types';
 
 export const ThemeProvider = ({ children, theme }: { children: React.ReactNode, theme: STheme }) => {
   const mergedTheme = deepmerge(
+    DEFAULT_THEME,
     theme,
-    LOCAL_THEME,
     { arrayMerge: (srcArray, overrideArray) => overrideArray }
   )
 
